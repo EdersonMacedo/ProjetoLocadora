85dbee53dff44138d082cd95d149f2394c406f6e
guest01 <guest01@10.1.49.100>
2013-09-30 10:11:37 -0300

    Adicionando a classe pessoa

---
+++ src/br/com/EdersonMacedo/model/Funcionario.java
@@ -0,0 +1,26 @@
+package br.com.EdersonMacedo.model;
+
+public class Funcionario extends Pessoa{
+    private String login;
+    private String senha;
+    public Funcionario(){
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
